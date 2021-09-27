4cf33f1490916b0fc3696cf7126c921b360ebd65
Mohit Pant <mohitpant1999@gmail.com>
2021-09-27 21:19:26 +0530

    Updates

---
+++ precache-manifest.e1e22b2cd56ea073e0f594b8e478d3df.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22cd0a4ba384405777dffc72ad484273",
+    "url": "/keeper/index.html"
+  },
+  {
+    "revision": "17da0b56a28d7c40a10c",
+    "url": "/keeper/static/js/2.33ec2792.chunk.js"
+  },
+  {
+    "revision": "4c970446cf6d76799d98",
+    "url": "/keeper/static/js/main.62b006ed.chunk.js"
+  },
+  {
+    "revision": "092074f7cd57a7d0f215",
+    "url": "/keeper/static/js/runtime-main.1e0fb4f8.js"
+  }
+]);
